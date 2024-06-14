e651261acdc6da94d3d1f96b9a0dff996716acf6
Techchhiv <techchivlim5@gmail.com>
2024-06-14 14:32:39 +0700

    fix create student

--- tests/Feature/StudentTest.php
+++ tests/Feature/StudentTest.php
@@ -3,8 +3,8 @@ use App\Models\Student;
 use function PHPUnit\Framework\assertTrue;
 test('create student', function () {
     $student = Student::create([
-        'name' => 'John Doe',
-        'email' => '<your_name>@itc.edu.kh',
+        'name' => 'Lim Techchhiv',
+        'email' => 'techchivlim5@itc.edu.kh',
         'phone' => '012 345 678',
         'dob' => '2003-10-01'
     ]);
